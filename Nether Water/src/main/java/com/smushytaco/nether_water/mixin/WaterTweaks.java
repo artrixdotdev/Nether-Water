33634096cb91eaf8abb3fb45846f47f845302410
artrixdotdev <sefu786@outlook.com>
2026-01-31 22:48:59 -0800

    1.21.11

--- Nether Water/src/main/java/com/smushytaco/nether_water/mixin/WaterTweaks.java
+++ Nether Water/src/main/java/com/smushytaco/nether_water/mixin/WaterTweaks.java
@@ -1,18 +1,36 @@
 package com.smushytaco.nether_water.mixin;
+
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.smushytaco.nether_water.NetherWater;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.attribute.EnvironmentAttribute;
+import net.minecraft.world.attribute.EnvironmentAttributeSystem;
+import net.minecraft.world.attribute.EnvironmentAttributes;
 import net.minecraft.world.item.BucketItem;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.dimension.DimensionType;
-import net.minecraft.world.phys.BlockHitResult;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+
 @Mixin(BucketItem.class)
 public abstract class WaterTweaks {
-    @WrapOperation(method = "emptyContents", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/dimension/DimensionType;ultraWarm()Z"))
-    private boolean hookPlaceFluid(DimensionType instance, Operation<Boolean> original, @Nullable LivingEntity user, Level world, BlockPos pos, @Nullable BlockHitResult hitResult) { return original.call(instance) && !NetherWater.INSTANCE.canHaveWater(pos.getY()); }
+
+    @WrapOperation(
+            method = "emptyContents",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/attribute/EnvironmentAttributeSystem;getValue(Lnet/minecraft/world/attribute/EnvironmentAttribute;Lnet/minecraft/core/BlockPos;)Ljava/lang/Object;"
+            )
+    )
+    private Object hookPlaceFluid(
+            EnvironmentAttributeSystem instance,
+            EnvironmentAttribute<?> attribute,
+            BlockPos pos,
+            Operation<Object> original
+    ) {
+        if (attribute == EnvironmentAttributes.WATER_EVAPORATES) {
+            boolean shouldEvaporate = (Boolean) original.call(instance, attribute, pos);
+            return shouldEvaporate && !NetherWater.INSTANCE.canHaveWater(pos.getY());
+        }
+        return original.call(instance, attribute, pos);
+    }
 }
